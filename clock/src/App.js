6bc9953c84cb6c987ccc5c4d6d0f5cffc14dd7bb
GaoGao0719 <31752971+GaoGao0719@users.noreply.github.com>
2019-05-20 11:21:42 -0400

    Change ComponentDidMount to ComponentWillMount, Set time range to 1000.

---
+++ clock/src/App.js
@@ -0,0 +1,42 @@
+//This is the second API debugging task.
+//The task is about React.js, which is similar to JavaScript.
+//In this task, we will use API to make a function worked.
+//We have a webpage. The function is show a worked clock,
+//which could help us calculate the time. (the number of the clock should move with time changing)
+
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  constructor() {
+    super()
+this.state={time:new Date()}
+  }
+
+  currentTime()
+  {
+    this.setState({
+      time: new Date()
+    })
+  }
+
+ componentDidMount() {
+      setInterval( () => {
+        this.setState({
+          curTime : new Date().toLocaleString()
+        })
+      },1000)
+    }
+
+
+  render() {
+
+    return (
+      <h1>
+        {this.state.time.toLocaleTimeString()}
+      </h1>
+    )
+  }
+}
+
+export default App;
